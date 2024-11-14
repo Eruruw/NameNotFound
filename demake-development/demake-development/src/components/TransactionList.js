924bedd5a4375ba767e1eda855f873f2b5ca49bb
WillWillson <reedj2715@gmail.com>
2024-11-13 22:36:37 -0600

    Small change
    Grabs from the database fix

--- demake-development/demake-development/src/components/TransactionList.js
+++ demake-development/demake-development/src/components/TransactionList.js
@@ -12,7 +12,7 @@ function TransactionList() {
         const fetchTransactions = async () => {
             if (currentUser) {
                 const transactionsSnapshot = await firestore
-                    .collection('profile')
+                    .collection('users')
                     .doc(currentUser.uid)
                     .collection('transactions')
                     .get();
